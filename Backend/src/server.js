27bdd647335521bce4fe329a0bde1d1ae7c94f6f
nguyenbaokhang2086 <nguyenbaokhang891@gmail.com>
2026-04-08 12:51:11 +0700

    feat: initialize Express server with CORS, routing, and Prisma dependencies

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 // src/server.js
 import express from "express";
+import cors from "cors"; // 1. Thêm dòng này
 import userRouter from "./routes/user.route.js";
 import dotenv from "dotenv";
 import { env } from "prisma/config";
@@ -8,9 +9,10 @@ import schoolRouter from "./routes/school.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001; // Thêm dự phòng cổng 3001 nếu .env chưa nhận
 
 // Middleware
+app.use(cors()); // 2. Thêm dòng này TRƯỚC các Routes
 app.use(express.json());
 
 // Routes
